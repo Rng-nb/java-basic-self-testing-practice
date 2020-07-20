ad390410e726dcbc9b9f615b64c28afd0f23a794
Rng-nb <18782824515@163.com>
2020-07-20 17:36:02 +0800

    [yafeng Wang] fix: fix ArrayTest

--- src/main/java/com/twc/javaBasic/MyStack.java
+++ src/main/java/com/twc/javaBasic/MyStack.java
@@ -24,7 +24,7 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-
+        storage[count++] = value;
         // --end-->
     }
 
@@ -37,7 +37,12 @@ public class MyStack {
         //  IMPORTANT: You SHOULD NOT USE COLLECTIONS (such as List<T>) OTHER THAN ARRAY.
         //
         // <--start
-
+        int[] storageTmp = new int[newCapacity];
+        for (int i = 0; i < count; ++i) {
+            storageTmp[i] = storage[i];
+        }
+        capacity = newCapacity;
+        storage= storageTmp;
         // --end-->
     }
 
@@ -57,7 +62,9 @@ public class MyStack {
         //  Please pop one element from the array. You should update the `count` field
         //  as well.
         // <--start
-
+        if(count > 0) {
+            return storage[--count];
+        }
         // --end-->
 
         throw new UnsupportedOperationException("Stack is empty.");
